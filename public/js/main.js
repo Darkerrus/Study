01f5c386d0aa491e0aaaf9e86cc1b704cef86ebd
Daniil Sukharnikov <afterallspace@gmail.com>
2023-05-18 22:00:59 +0600

    idi nahui

--- public/js/main.js
+++ public/js/main.js
@@ -2,19 +2,49 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      data: []
+      data: [],
+      filteredData: [],
+      pNode: {}
     };
   },
-  computed: {
-    
-  },
   methods: {
+    toDefault() {
+      this.filteredData = []
+      while (this.pNode.hasChildNodes()){
+        this.pNode.removeChild(el.lastChild);
+      }
+    },
+    getAll() {
+      toDefault()
+      this.filteredData = this.data
+    },
     getDogs() {
-
+      toDefault()
+      this.data.forEach(el => {
+        if(el.dataset.kind === 'Dog') {
+          this.filteredData.push(el)
+        }
+      })
+    },
+    getCats() {
+      toDefault()
+      this.data.forEach(el => {
+        if(el.dataset.kind === 'Cat') {
+          this.filteredData.push(el)
+        }
+      })
+    },
+    getBird() {
+      toDefault()
+      this.data.forEach(el => {
+        if(el.dataset.kind === 'Bird') {
+          this.filteredData.push(el)
+        }
+      })
     }
   },
   mounted() {
     this.data = document.querySelectorAll('[data-kind]')
-    console.log(this.data);
+    this.pNode = document.getElementById('.animals');
   },
 }).mount("#app");
